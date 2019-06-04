848a11924d40dd797e8fec3195d7061a6b035a11
Ming <john00850329@gmail.com>
2019-06-04 14:22:05 +0800

    yaaa

--- 1_unprivileged/asm_func.s
+++ 1_unprivileged/asm_func.s
@@ -2,24 +2,39 @@
 
 .global	read_sp
 read_sp:
-	??????
+	mrs r0, sp ; 
+	bx lr ;
 
 .global	read_msp
 read_msp:
-	??????
+	mrs r0, msp ;
+	bx lr ; 
 
 .global	read_psp
 read_psp:
-	??????
+	mrs r0, psp ;
+	bx lr ; 
 
 .global	read_ctrl
 read_ctrl:
-	??????
+	mrs r0, CONTROL ;
+	bx lr ;
 
 .global	start_user
 start_user:
-	??????
+	movs lr, r0 ;
+	msr psp, r1 ;
+
+	movs r3, 0b11 ;
+	msr CONTROL, r3 ;
+	isb ;
+
+	bx lr ;
 
 .global	sw_priv
 sw_priv:
-	??????
+	movs r4, 0b11 ;
+	msr CONTROL, r4 ;
+	isb ;
+
+	bx lr ;
--- 1_unprivileged/main.c
+++ 1_unprivileged/main.c
@@ -26,7 +26,7 @@ int main(void)
 	printf("[Kernel] Switch to unprivileged thread mode & start user task (psp_init = 0x%x).\r\n\n", (unsigned int)psp_init);
 
 	//start user task
-	??????
+	start_user((unit32_t *)user_task,psp_int);
 
 	while (1) //should not go here
 		;
